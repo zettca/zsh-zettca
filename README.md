a92b53037bfed2bf2ae8621fd19ed85520f47619
Bruno Henriques <zettca@gmail.com>
2023-01-27 01:49:47 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# zsh-zettca
+
+`oh-my-zsh` plugin with some of my stuff
+
+Made for GNU/Linux. If you're using MacOS, you'll need something like [coreutils](https://formulae.brew.sh/formula/coreutils)
---
+++ zettca.plugin.zsh
@@ -0,0 +1,12 @@
+alias l='ls -C --group-directories-first'
+alias la='ls -A --group-directories-first'
+alias ll='ls -lGh --group-directories-first --sort=extension'
+
+alias de='cd ~/Desktop'
+alias dl='cd ~/Downloads'
+
+alias py='python3'
+alias o='xdg-open'
+alias dudu='du -h --max-depth=1 .'
+
+alias nra='npm-run-all'
